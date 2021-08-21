778423e073e9e6e553df9bb3d584fb444c0905cf
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-08-21 17:13:54 +0700

    add component back button

---
+++ src/components/BackButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const BackButton = props => {
+  return (
+    <>
+      <TouchableOpacity onPress={props.func}>
+        <MaterialIcons name="arrow-back-ios" color="#000" size={30} />
+      </TouchableOpacity>
+    </>
+  );
+};
+
+export default BackButton;
