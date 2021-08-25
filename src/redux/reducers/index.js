1b8ac9c1f6dcae555d38d18ed7dbd2906fd191ef
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-08-25 15:52:09 +0700

    update minor

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,6 +3,8 @@ import {combineReducers} from 'redux';
 import {persistReducer} from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import auth from './auth';
+import user from './user';
+import destination from './destination';
 
 const persistAuth = {
   key: 'auth',
@@ -11,6 +13,8 @@ const persistAuth = {
 
 const rootReducer = combineReducers({
   auth: persistReducer(persistAuth, auth),
+  user,
+  destination,
 });
 
 export default rootReducer;
