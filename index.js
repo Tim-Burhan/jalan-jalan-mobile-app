8ccf5b42cfc95dedef2e5d2083ab83ec7b93c094
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-08-25 15:07:08 +0700

    add redux

--- index.js
+++ index.js
@@ -2,7 +2,24 @@
  * @format
  */
 
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-AppRegistry.registerComponent(appName, () => App);
+import {Provider} from 'react-redux';
+import reduxConfig from './src/redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+
+const redux = reduxConfig();
+
+const Main = () => {
+  return (
+    <Provider store={redux.store}>
+      <PersistGate persistor={redux.persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Main);
