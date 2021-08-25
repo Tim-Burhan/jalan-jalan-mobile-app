48823a2b7a4f656895c7f688cc9e1f8a44fb32c1
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-08-25 15:07:32 +0700

    add auth redux

---
+++ src/redux/actions/auth.js
@@ -0,0 +1,63 @@
+import {http} from '../../helpers/http';
+import {REACT_APP_BASE_URL} from '@env';
+
+export const authLogin = (email, password) => async dispatch => {
+  console.log(REACT_APP_BASE_URL);
+  const form = new URLSearchParams();
+  form.append('email', email);
+  form.append('password', password);
+  console.log(form.toString());
+  try {
+    const {data} = await http().post(
+      `${REACT_APP_BASE_URL}/auth/login`,
+      form.toString(),
+    );
+    dispatch({
+      type: 'AUTH_LOGIN',
+      payload: data,
+    });
+    setTimeout(() => {
+      dispatch({type: 'AUTH_RESET'});
+    }, 2000);
+  } catch (err) {
+    dispatch({
+      type: 'AUTH_LOGIN_FAILED',
+      payload: err.response.data.message,
+    });
+    setTimeout(() => {
+      dispatch({type: 'AUTH_RESET'});
+    }, 2000);
+  }
+};
+
+export const authRegister = (name, email, password) => async dispatch => {
+  const form = new URLSearchParams();
+  form.append('name', name);
+  form.append('email', email);
+  form.append('password', password);
+  try {
+    const {data} = await http().post(
+      `${REACT_APP_BASE_URL}/auth/register`,
+      form.toString(),
+    );
+    dispatch({
+      type: 'AUTH_REGISTER',
+      payload: data.message,
+    });
+    setTimeout(() => {
+      dispatch({type: 'AUTH_RESET'});
+    }, 2000);
+  } catch (err) {
+    dispatch({
+      type: 'AUTH_REGISTER_FAILED',
+      payload: err.response.data.message,
+    });
+    setTimeout(() => {
+      dispatch({type: 'AUTH_RESET'});
+    }, 2000);
+  }
+};
+
+export const authLogout = () => ({
+  type: 'AUTH_LOGOUT',
+});
